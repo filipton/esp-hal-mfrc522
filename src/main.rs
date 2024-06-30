72cd93dec4c76300b63ce9e2c40306e6ca456ca7
filipton <filipton12@gmail.com>
2024-06-30 15:21:07 +0200

    feat: embassy (async)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
+    clock::ClockControl, gpio::Io, peripherals::Peripherals, prelude::*, system::SystemControl,
+    timer::timg::TimerGroup,
 };
 
-#[entry]
-fn main() -> ! {
+#[main]
+async fn main(_spawner: Spawner) {
     let peripherals = Peripherals::take();
     let system = SystemControl::new(peripherals.SYSTEM);
-
     let clocks = ClockControl::max(system.clock_control).freeze();
-    let delay = Delay::new(&clocks);
-
     esp_println::logger::init_logger_from_env();
+
+    let timg0 = TimerGroup::new_async(peripherals.TIMG0, &clocks);
+    esp_hal_embassy::init(&clocks, timg0);
+
+    /*
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let sck = io.pins.gpio4;
+    let miso = io.pins.gpio2;
+    let mosi = io.pins.gpio3;
+    let cs = io.pins.gpio5;
+    */
 
     loop {
         log::info!("Hello world!");
-        delay.delay(500.millis());
+        Timer::after(Duration::from_millis(500)).await;
     }
 }
