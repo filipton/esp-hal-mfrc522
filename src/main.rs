3a3cd7ccaaae5a58f8a9ab290c5a5d9b28d2f803
filipton <filipton12@gmail.com>
2024-06-30 20:35:45 +0200

    chore: move all functions to MFRC522 struct

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ async fn main(_spawner: Spawner) {
     let sck = io.pins.gpio4;
     let miso = io.pins.gpio2;
     let mosi = io.pins.gpio3;
-    let mut cs = Output::new(io.pins.gpio5, Level::High);
+    let cs = Output::new(io.pins.gpio5, Level::High);
 
     let dma = Dma::new(peripherals.DMA);
     let dma_chan = dma.channel0;
@@ -53,259 +53,245 @@ async fn main(_spawner: Spawner) {
 
     let spi = Spi::new(peripherals.SPI3, 4.MHz(), SpiMode::Mode0, &clocks);
     let spi: Spi<SPI3, FullDuplexMode> = spi.with_sck(sck).with_miso(miso).with_mosi(mosi);
-    let mut spi: SpiDma<SPI3, _, FullDuplexMode, Async> = spi.with_dma(dma_chan);
+    let spi: SpiDma<SPI3, _, FullDuplexMode, Async> = spi.with_dma(dma_chan);
 
-    esp_println::println!("PCD reset first");
-    _ = write_register(
-        &mut spi,
-        &mut cs,
-        PCDRegister::CommandReg,
-        PCDCommand::SoftReset,
-    )
-    .await;
+    let mut mfrc522 = MFRC522::new(spi, cs);
+    _ = mfrc522.pcd_init().await;
+    esp_println::println!("PCD ver: {:?}", mfrc522.pcd_get_version().await);
+}
 
-    // max 3 tries
-    for _ in 0..3 {
-        let out = read_register(&mut spi, &mut cs, PCDRegister::CommandReg).await;
-        if let Ok(out) = out {
-            let out = out & (1 << 4);
-            if out == 0 {
+pub struct MFRC522<S, C>
+where
+    S: embedded_hal_async::spi::SpiBus,
+    C: OutputPin,
+{
+    spi: S,
+    cs: C,
+    read_buff: [u8; 1],
+}
+
+// TODO: implement own errors type (using for example thiserror-no-std or snafu)
+impl<S, C> MFRC522<S, C>
+where
+    S: embedded_hal_async::spi::SpiBus,
+    C: OutputPin,
+{
+    pub fn new(spi: S, cs: C) -> Self {
+        Self {
+            spi,
+            cs,
+            read_buff: [0],
+        }
+    }
+
+    pub async fn pcd_init(&mut self) -> Result<(), ()> {
+        self.pcd_reset().await?;
+
+        self.write_reg(PCDRegister::TxModeReg, 0x00).await?;
+        self.write_reg(PCDRegister::RxModeReg, 0x00).await?;
+
+        self.write_reg(PCDRegister::ModWidthReg, 0x26).await?;
+
+        self.write_reg(PCDRegister::TModeReg, 0x80).await?;
+        self.write_reg(PCDRegister::TPrescalerReg, 0xA9).await?;
+        self.write_reg(PCDRegister::TReloadRegH, 0x03).await?;
+        self.write_reg(PCDRegister::TReloadRegL, 0xE8).await?;
+
+        self.write_reg(PCDRegister::TxASKReg, 0x40).await?;
+        self.write_reg(PCDRegister::ModeReg, 0x3D).await?;
+
+        self.pcd_antenna_on().await?;
+        Timer::after(Duration::from_millis(4)).await;
+
+        Ok(())
+    }
+
+    pub async fn pcd_reset(&mut self) -> Result<(), ()> {
+        self.write_reg(PCDRegister::CommandReg, PCDCommand::SoftReset)
+            .await?;
+
+        // max 3 tries
+        for _ in 0..3 {
+            let out = self.read_reg(PCDRegister::CommandReg).await;
+            if let Ok(out) = out {
+                let out = out & (1 << 4);
+                if out == 0 {
+                    break;
+                }
+            }
+
+            Timer::after(Duration::from_millis(50)).await;
+        }
+
+        Ok(())
+    }
+
+    pub async fn pcd_antenna_on(&mut self) -> Result<(), ()> {
+        let val = self.read_reg(PCDRegister::TxControlReg).await?;
+        if (val & 0x03) != 0x03 {
+            self.write_reg(PCDRegister::TxControlReg, val | 0x03)
+                .await?;
+        }
+
+        Ok(())
+    }
+
+    pub async fn pcd_get_version(&mut self) -> Result<u8, ()> {
+        self.read_reg(PCDRegister::VersionReg).await
+    }
+
+    /// Now it prints data to console, TODO: change this
+    /// Always returns false (for now)
+    pub async fn pcd_selftest(&mut self) -> Result<bool, ()> {
+        self.write_reg(PCDRegister::FIFOLevelReg, 0x80).await?;
+        self.write_reg_buff(PCDRegister::FIFODataReg, 25, &[0; 25])
+            .await?;
+
+        self.write_reg(PCDRegister::CommandReg, PCDCommand::Mem)
+            .await?;
+        self.write_reg(PCDRegister::AutoTestReg, 0x09).await?;
+        self.write_reg(PCDRegister::FIFODataReg, 0x00).await?;
+        self.write_reg(PCDRegister::CommandReg, PCDCommand::CalcCRC)
+            .await?;
+
+        for _ in 0..0xFF {
+            let n = self.read_reg(PCDRegister::FIFOLevelReg).await?;
+            if n >= 64 {
                 break;
             }
         }
 
-        Timer::after(Duration::from_millis(50)).await;
-    }
+        self.write_reg(PCDRegister::CommandReg, PCDCommand::Idle)
+            .await?;
 
-    esp_println::println!("PCD init");
-    _ = write_register(&mut spi, &mut cs, PCDRegister::TxModeReg, 0x00).await;
-    _ = write_register(&mut spi, &mut cs, PCDRegister::RxModeReg, 0x00).await;
+        let mut res = [0; 64];
+        self.read_reg_buff(PCDRegister::FIFODataReg, 64, &mut res, 0)
+            .await?;
+        self.write_reg(PCDRegister::AutoTestReg, 0x40 & 0x00)
+            .await?;
 
-    _ = write_register(&mut spi, &mut cs, PCDRegister::ModWidthReg, 0x26).await;
+        for i in 0..64 {
+            if i % 8 == 0 {
+                esp_println::print!("\n");
+            }
 
-    _ = write_register(&mut spi, &mut cs, PCDRegister::TModeReg, 0x80).await;
-    _ = write_register(&mut spi, &mut cs, PCDRegister::TPrescalerReg, 0xA9).await;
-    _ = write_register(&mut spi, &mut cs, PCDRegister::TReloadRegH, 0x03).await;
-    _ = write_register(&mut spi, &mut cs, PCDRegister::TReloadRegL, 0xE8).await;
-
-    _ = write_register(&mut spi, &mut cs, PCDRegister::TxASKReg, 0x40).await;
-    _ = write_register(&mut spi, &mut cs, PCDRegister::ModeReg, 0x3D).await;
-
-    esp_println::println!("PCD antenna on");
-    let val = read_register(&mut spi, &mut cs, PCDRegister::TxControlReg)
-        .await
-        .unwrap();
-
-    if (val & 0x03) != 0x03 {
-        _ = write_register(&mut spi, &mut cs, PCDRegister::TxControlReg, val | 0x03).await;
-    }
-
-    Timer::after(Duration::from_millis(4)).await;
-    let ver = read_register(&mut spi, &mut cs, PCDRegister::VersionReg)
-        .await
-        .unwrap();
-
-    esp_println::println!("PCD ver: {ver:?}");
-
-    // SELFTEST CODE
-    /*
-    esp_println::println!("PCD selftest");
-
-    _ = write_register(&mut spi, &mut cs, PCDRegister::FIFOLevelReg, 0x80).await;
-    _ = write_registers(
-        &mut spi,
-        &mut cs,
-        PCDRegister::FIFODataReg,
-        25,
-        &[
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        ],
-    )
-    .await;
-
-    _ = write_register(&mut spi, &mut cs, PCDRegister::CommandReg, PCDCommand::Mem).await;
-    _ = write_register(&mut spi, &mut cs, PCDRegister::AutoTestReg, 0x09).await;
-    _ = write_register(&mut spi, &mut cs, PCDRegister::FIFODataReg, 0x00).await;
-    _ = write_register(
-        &mut spi,
-        &mut cs,
-        PCDRegister::CommandReg,
-        PCDCommand::CalcCRC,
-    )
-    .await;
-
-    for _ in 0..0xFF {
-        let n = read_register(&mut spi, &mut cs, PCDRegister::FIFOLevelReg)
-            .await
-            .unwrap();
-
-        if n >= 64 {
-            break;
+            esp_println::print!("{:#04x} ", res[i]);
         }
+        esp_println::print!("\n");
+
+        self.pcd_init().await?;
+        Ok(false)
     }
 
-    _ = write_register(&mut spi, &mut cs, PCDRegister::CommandReg, PCDCommand::Idle).await;
+    pub async fn write_reg(&mut self, reg: u8, val: u8) -> Result<(), ()> {
+        _ = self.cs.set_low();
+        _ = self.spi.transfer(&mut self.read_buff, &[reg << 1]).await;
+        _ = self.spi.transfer(&mut self.read_buff, &[val]).await;
+        _ = self.cs.set_high();
 
-    let mut res = [0; 64];
-    _ = read_registers(&mut spi, &mut cs, PCDRegister::FIFODataReg, 64, &mut res, 0).await;
-    _ = write_register(&mut spi, &mut cs, PCDRegister::AutoTestReg, 0x40 & 0x00).await;
+        Ok(())
+    }
 
-    for i in 0..64 {
-        if i % 8 == 0 {
-            esp_println::print!("\n");
+    pub async fn write_reg_buff(&mut self, reg: u8, count: usize, values: &[u8]) -> Result<(), ()> {
+        _ = self.cs.set_low();
+        _ = self.spi.transfer(&mut self.read_buff, &[reg << 1]).await;
+
+        for i in 0..count {
+            _ = self.spi.transfer(&mut self.read_buff, &[values[i]]).await;
         }
 
-        esp_println::print!("{:#04x} ", res[i]);
-    }
-    esp_println::print!("\n");
-    */
-}
-
-async fn write_register(
-    spi: &mut impl embedded_hal_async::spi::SpiBus,
-    cs: &mut impl OutputPin,
-    register: u8,
-    value: u8,
-) -> Result<(), ()> {
-    let mut read_buff = [0; 1];
-
-    _ = cs.set_low();
-    _ = spi.transfer(&mut read_buff, &[register << 1]).await;
-
-    _ = spi.transfer(&mut read_buff, &[value]).await;
-    _ = cs.set_high();
-
-    Ok(())
-}
-
-async fn write_registers(
-    spi: &mut impl embedded_hal_async::spi::SpiBus,
-    cs: &mut impl OutputPin,
-    register: u8,
-    count: usize,
-    values: &[u8],
-) -> Result<(), ()> {
-    let mut read_buff = [0; 1];
-
-    _ = cs.set_low();
-    _ = spi.transfer(&mut read_buff, &[register << 1]).await;
-
-    for i in 0..count {
-        _ = spi.transfer(&mut read_buff, &[values[i]]).await;
+        _ = self.cs.set_high();
+        Ok(())
     }
 
-    _ = cs.set_high();
+    pub async fn read_reg(&mut self, reg: u8) -> Result<u8, ()> {
+        _ = self.cs.set_low();
+        _ = self
+            .spi
+            .transfer(&mut self.read_buff, &[(reg << 1) | 0x80])
+            .await;
 
-    Ok(())
-}
+        _ = self.spi.transfer(&mut self.read_buff, &[0]).await;
+        _ = self.cs.set_high();
 
-// TODO: use ? to propagate errors up
-async fn read_register(
-    spi: &mut impl embedded_hal_async::spi::SpiBus,
-    cs: &mut impl OutputPin,
-    register: u8,
-) -> Result<u8, ()> {
-    let mut read_buff = [0; 1];
-
-    _ = cs.set_low();
-    _ = spi
-        .transfer(&mut read_buff, &[(register << 1) | 0x80])
-        .await;
-
-    _ = spi.transfer(&mut read_buff, &[0]).await;
-    _ = cs.set_high();
-
-    Ok(read_buff[0])
-}
-
-async fn read_registers(
-    spi: &mut impl embedded_hal_async::spi::SpiBus,
-    cs: &mut impl OutputPin,
-    register: u8,
-    count: usize,
-    buff: &mut [u8],
-    rx_align: u8,
-) -> Result<(), ()> {
-    if count == 0 {
-        return Ok(());
+        Ok(self.read_buff[0])
     }
 
-    let addr = 0x80 | (register << 1);
-    let mut index = 0;
-    let mut read_buff = [0; 1];
-
-    _ = cs.set_low();
-    _ = spi.transfer(&mut read_buff, &[addr]).await;
-    if rx_align > 0 {
-        let mask = (0xFF << rx_align) & 0xFF;
-        _ = spi.transfer(&mut read_buff, &[addr]).await;
-        let val = read_buff[0];
-
-        buff[0] = (buff[0] & !mask) | (val & mask);
-        index += 1;
-    }
-
-    while index < count - 1 {
-        _ = spi.transfer(&mut read_buff, &[addr]).await;
-        buff[index] = read_buff[0];
-        index += 1;
-    }
-
-    _ = spi.transfer(&mut read_buff, &[0]).await;
-    buff[index] = read_buff[0];
-
-    _ = cs.set_high();
-    Ok(())
-}
-
-async fn clear_register_bit_mask(
-    spi: &mut impl embedded_hal_async::spi::SpiBus,
-    cs: &mut impl OutputPin,
-    register: u8,
-    mask: u8,
-) -> Result<(), ()> {
-    let tmp = read_register(spi, cs, register).await?;
-    write_register(spi, cs, register, tmp & (!mask)).await?;
-
-    Ok(())
-}
-
-async fn set_register_bit_mask(
-    spi: &mut impl embedded_hal_async::spi::SpiBus,
-    cs: &mut impl OutputPin,
-    register: u8,
-    mask: u8,
-) -> Result<(), ()> {
-    let tmp = read_register(spi, cs, register).await?;
-    write_register(spi, cs, register, tmp | mask).await?;
-
-    Ok(())
-}
-
-async fn calc_crc(
-    spi: &mut impl embedded_hal_async::spi::SpiBus,
-    cs: &mut impl OutputPin,
-    data: &[u8],
-    length: u8,
-    res: &mut [u8],
-) -> Result<(), ()> {
-    write_register(spi, cs, PCDRegister::CommandReg, PCDCommand::Idle).await?;
-    write_register(spi, cs, PCDRegister::DivIrqReg, 0x04).await?;
-    write_register(spi, cs, PCDRegister::FIFOLevelReg, 0x80).await?;
-    write_registers(spi, cs, PCDRegister::FIFODataReg, length as usize, data).await?;
-    write_register(spi, cs, PCDRegister::CommandReg, PCDCommand::CalcCRC).await?;
-
-    for i in (0..5000).rev() {
-        let n = read_register(spi, cs, PCDRegister::DivIrqReg).await?;
-        if n & 0x04 != 0 {
-            write_register(spi, cs, PCDRegister::CommandReg, PCDCommand::Idle).await?;
-
-            res[0] = read_register(spi, cs, PCDRegister::CRCResultRegL).await?;
-            res[1] = read_register(spi, cs, PCDRegister::CRCResultRegH).await?;
+    pub async fn read_reg_buff(
+        &mut self,
+        reg: u8,
+        count: usize,
+        output_buff: &mut [u8],
+        rx_align: u8,
+    ) -> Result<(), ()> {
+        if count == 0 {
             return Ok(());
         }
+
+        let addr = 0x80 | (reg << 1);
+        let mut index = 0;
+
+        _ = self.cs.set_low();
+        _ = self.spi.transfer(&mut self.read_buff, &[addr]).await;
+        if rx_align > 0 {
+            let mask = (0xFF << rx_align) & 0xFF;
+            _ = self.spi.transfer(&mut self.read_buff, &[addr]).await;
+
+            output_buff[0] = (output_buff[0] & !mask) | (self.read_buff[0] & mask);
+            index += 1;
+        }
+
+        while index < count - 1 {
+            _ = self.spi.transfer(&mut self.read_buff, &[addr]).await;
+            output_buff[index] = self.read_buff[0];
+            index += 1;
+        }
+
+        _ = self.spi.transfer(&mut self.read_buff, &[0]).await;
+        output_buff[index] = self.read_buff[0];
+
+        _ = self.cs.set_high();
+        Ok(())
     }
 
-    // timeout?
-    Err(())
+    pub async fn clear_register_bit_mask(&mut self, reg: u8, mask: u8) -> Result<(), ()> {
+        let tmp = self.read_reg(reg).await?;
+        self.write_reg(reg, tmp & (!mask)).await?;
+
+        Ok(())
+    }
+
+    pub async fn set_register_bit_mask(&mut self, reg: u8, mask: u8) -> Result<(), ()> {
+        let tmp = self.read_reg(reg).await?;
+        self.write_reg(reg, tmp | mask).await?;
+
+        Ok(())
+    }
+
+    pub async fn calc_crc(&mut self, data: &[u8], length: u8, res: &mut [u8]) -> Result<(), ()> {
+        self.write_reg(PCDRegister::CommandReg, PCDCommand::Idle)
+            .await?;
+        self.write_reg(PCDRegister::DivIrqReg, 0x04).await?;
+        self.write_reg(PCDRegister::FIFOLevelReg, 0x80).await?;
+        self.write_reg_buff(PCDRegister::FIFODataReg, length as usize, data)
+            .await?;
+        self.write_reg(PCDRegister::CommandReg, PCDCommand::CalcCRC)
+            .await?;
+
+        // TODO: make sth like timeout (this 5000 is only for waiting)
+        for _ in 0..5000 {
+            let n = self.read_reg(PCDRegister::DivIrqReg).await?;
+            if n & 0x04 != 0 {
+                self.write_reg(PCDRegister::CommandReg, PCDCommand::Idle)
+                    .await?;
+
+                res[0] = self.read_reg(PCDRegister::CRCResultRegL).await?;
+                res[1] = self.read_reg(PCDRegister::CRCResultRegH).await?;
+                return Ok(());
+            }
+        }
+
+        // timeout?
+        Err(())
+    }
 }
